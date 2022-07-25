3de0d69e6fdbb433952045e8a8ce7a13c724f5fb
Sanchuniathon <leeerisman@gmail.com>
2022-07-24 22:52:54 -0600

    Add logic for range, limiting movement, attacking, sound

--- src/Game.js
+++ src/Game.js
@@ -30,26 +30,52 @@ export default class Game {
         }
     }
     checkForValidMove(i){
+        //first we block deployment row
         if((i>2) && (i<15)){
+            //Can't go into an occupied square
             if(this.squares[i].value == null){
-                return true;              
+                //Can only move one space at a time
+                var checkLocation = Math.abs(this.selectedSquareIndex - i);
+                if((checkLocation==3)||(checkLocation==1)){
+                    return true;     
+                }                        
             }           
         }
         return false;
     }
+    checkForAttack(i){
+        if((this.squares[i].value != this.currentTurn)&&(this.squares[i].value != null)){
+            //Can only shoot one space at a time
+            var checkLocation = Math.abs(this.selectedSquareIndex - i);
+            if((checkLocation==3)||(checkLocation==1)){
+                return true;     
+            }
+        }
+        return false;
+    }
+    completeTurn(){   
+        this.movesMade++;
+        this.checkForWinner();
+        this.currentTurn = (this.currentTurn === Game.O) ? Game.X : Game.O; //if it is O's turn set to X's turn, otherwise set it to O's turn
+    }
 
     makeMove(i){
+        //we don't have a tile selected
         if(this.inProgress && !this.selectionMade){
             this.checkForValidSelection(i);
-        } 
+        }
+        //If there is already a selection made then we can try to make a move or attack
         else if(this.inProgress && this.selectionMade){
             if(this.checkForValidMove(i)){
+                //move normally
                 this.squares[i].value = this.currentTurn;
                 this.squares[this.selectedSquareIndex].value = null;
-                //attackSound.play();
-                this.movesMade++;
-                this.checkForWinner();
-                this.currentTurn = (this.currentTurn === Game.O) ? Game.X : Game.O; //if it is O's turn set to X's turn, otherwise set it to O's turn
+                this.completeTurn();
+            }
+            //We can't move there but maybe we can attack there
+            else if(this.checkForAttack(i)){
+                attackSound.play();
+                this.completeTurn();
             }
             //move is not valid
             else{
